b675702563db588f893578fce5bd4984d508b099
thuan.ngo <thuan.ngo@eastagile.com>
2021-03-21 02:20:11 +0700

    [f] Style upload pic page

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -1,7 +1,37 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  spacing: 8,
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const Upload = () => {
+  const classes = useStyles();
+
   const [selectedImg, setSelectedImg] = useState(null);
 
   const handleFileChange = (e) => {
@@ -26,16 +56,32 @@ const Upload = () => {
   };
 
   return (
-    <div>
-      <form
-      // action="localhost:3001/pictures/upload"
-      // method="post"
-      // encType="multipart/form-data"
-      >
-        <input type="file" name="myImage" onChange={handleFileChange} />
-        <button onClick={handleSubmit}>submit</button>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Upload your favorite picture 📸
+        </Typography>
+        <Box m={2} />
+
+        <form
+        // action="localhost:3001/pictures/upload"
+        // method="post"
+        // encType="multipart/form-data"
+        >
+          <input type="file" name="myImage" onChange={handleFileChange} />
+          <Box m={2} />
+          <Button
+            onClick={handleSubmit}
+            color="primary"
+            variant="contained"
+            component="span"
+          >
+            Upload Picture
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 };
 
